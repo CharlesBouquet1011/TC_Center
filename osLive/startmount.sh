6a3c07901d52745e6722ac59a8d1ae7469ef8341
cb <charlesbouquet1011@gmail.com>
2025-04-16 14:29:00 +0200

    patch

--- osLive/startmount.sh
+++ osLive/startmount.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-if [ ! -d "/mnt/home" ]; then
-  sudo mkdir /mnt/home
-  sudo mount -t cifs -o username=$USER_LOGIN,uid=$USER_LOGIN,gid=dsi //ps-home.insa-lyon.fr/$USER_LOGIN /mnt/home
-  if [ $? -eq 0 ]; then
-    sudo ln -s /mnt/home 
-  else
-    echo "Le montage a échoué"
-    sudo rmdir /mnt/home
-  fi
-fi
-
 
 if [! -d "/mnt/k3sVolume"]; then
   sudo mkdir /mnt/k3sVolume
