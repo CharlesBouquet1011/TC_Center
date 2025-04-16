79739b859abb63ca0c9d23c71f7c15432306f214
cb <charlesbouquet1011@gmail.com>
2025-04-16 22:21:02 +0200

    nettoyage

--- osLive/hostname-k3s.sh
+++ osLive/hostname-k3s.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Récupère la première IP non-loopback (en adaptant si tu veux une interface précise)
-IP=$(ip -4 addr show scope global | grep inet | awk '{print $2}' | cut -d/ -f1 | head -n1) #merci chat gpt
-
-# Cherche le hostname associé à cette IP dans /etc/hosts
-
-
 # Vérifie qu'on a bien un hostname
 while true; do
     HOSTNAME=$(systemd-ask-password --echo "Nom du noeud (nodeXX avec XX le numéro du noeud, 01, 02 etc)")
