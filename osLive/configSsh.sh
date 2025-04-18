57c6f600d591b77814077f697cc2655fa3e525b5
cb <charlesbouquet1011@gmail.com>
2025-04-18 14:37:14 +0200

    prevoyance

--- osLive/configSsh.sh
+++ osLive/configSsh.sh
@@ -3,6 +3,8 @@ MOUNT_POINT="/mnt/config"
 CONF_PATH="$MOUNT_POINT/authorized_keys"
 TARGET="/home/user/.ssh/authorized_keys"
 
+mkdir /home/user/.ssh
+chmod 700 /home/user/.ssh
 # Monter la partition par label
 if [ -b /dev/disk/by-label/CONFIG ]; then
     mkdir -p $MOUNT_POINT
@@ -11,6 +13,7 @@ fi
 
 if [ -f "$CONF_PATH" ]; then
     cp "$CONF_PATH" "$TARGET"
+    chmod 600 "$TARGET"
     echo "[update-hosts] /home/user/.ssh/authorized_keys remplacé avec succès."
 else
     echo "[update-hosts] Aucun fichier hosts.conf trouvé."
