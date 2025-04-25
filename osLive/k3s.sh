15b29d70e15f5e4c6895b81e8468ac96ef431df0
cb <charlesbouquet1011@gmail.com>
2025-04-25 22:16:39 +0200

    correction k3s

--- osLive/k3s.sh
+++ osLive/k3s.sh
@@ -8,6 +8,10 @@ ROLE=$(systemd-ask-password --echo "Quel rôle pour ce nœud ? (master/agent)")
 echo "INFO: Récupération k3s"
 echo ""
 curl -sfL https://get.k3s.io -o ./install-k3s.sh
+if [ $? -ne 0 ]; then
+    echo "ERREUR: Échec du téléchargement du script d'installation k3s."
+    exit 1
+fi
 chmod +x ./install-k3s.sh
 
 echo "INFO: Installation k3s en tant que ($ROLE)"
@@ -15,6 +19,9 @@ if [ "$ROLE" = "master" ]; then
     INSTALL_K3S_SKIP_ENABLE=true \
     INSTALL_K3S_EXEC="--snapshotter=fuse-overlayfs --token TC-Center" \
     ./install-k3s.sh
+
+    sudo systemctl start k3s #lancer k3s comme service
+    sudo chown user:user /etc/rancher/k3s/k3s.yaml #mettre ce fichier à user pour que le kubectl get nodes fonctionne
 
 elif [ "$ROLE" = "agent" ]; then
     MASTER_IP=$(systemd-ask-password --echo "IP du serveur master :")
@@ -28,6 +35,8 @@ elif [ "$ROLE" = "agent" ]; then
     K3S_TOKEN=TC-Center \
     INSTALL_K3S_EXEC="agent --snapshotter=fuse-overlayfs" \
     ./install-k3s.sh
+
+    sudo systemctl start k3s-agent
 else
     echo "ERREUR: ROLE doit être 'master' ou 'agent'."
     exit 1
@@ -37,5 +46,3 @@ rm ./install-k3s.sh
 
 ./hostname-k3s.sh
 
-sudo systemctl start k3s #lancer k3s comme service
-sudo chown user:user /etc/rancher/k3s/k3s.yaml #mettre ce fichier à user pour que le kubectl get nodes fonctionne
