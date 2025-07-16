f649911aef1fa5cca87d39dea24fa179eaa61613
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2025-07-16 17:05:22 +0200

    Simplification de scripts

--- osLive/startmount.sh
+++ osLive/startmount.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-
-mkdir -p /mnt/k3sVolume
-  
-if  mount /dev/sda /mnt/k3sVolume ; then
+if  mount /dev/sda1 /mnt/ ; then
   echo "Disque monté avec succès"
   #création des dossiers k3s et docker
   mkdir -p /mnt/k3sVolume/docker
   mkdir -p /mnt/k3sVolume/k3s 
 else
-  echo "Échec du montage"
-  sudo rmdir /mnt/k3sVolume
-  fi
+  echo "Echec du montage"
+fi
 
